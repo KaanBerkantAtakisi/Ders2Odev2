f2b36ac168df7bafcfe6ad7b57daceef3f2cc42a
KaanBerkantAtakisi <kaanatakisi75@gmail.com>
2024-02-17 14:45:43 +0300

    Add project files.

---
+++ ThreadinDemo2/Form1.cs
@@ -0,0 +1,20 @@
+namespace ThreadinDemo2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ýþlem 1 çalýþtý");
+        }
+
+        private void btnProcess2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ýþlem 2 çalýþtý");
+        }
+    }
+}
